47e28c092a0f62f891592c6207bc2aafc4f0ca22
nidraoo <05nidzzzzz@gmail.com>
2025-01-30 00:45:31 +0530

    updated table

--- Frontend/components/ui/table.tsx
+++ Frontend/components/ui/table.tsx
@@ -1,47 +1,25 @@
 import * as React from "react";
 import clsx from "clsx"; // Import clsx for merging class names
 
-interface TableProps extends React.HTMLProps<HTMLTableElement> {
-  className?: string;
-}
-
-interface TableHeadProps extends React.HTMLProps<HTMLTableSectionElement> {
-  className?: string;
-}
-
-interface TableRowProps extends React.HTMLProps<HTMLTableRowElement> {
-  className?: string;
-}
-
-interface TableHeaderProps extends React.HTMLProps<HTMLTableCellElement> {
-  className?: string;
-}
-
-interface TableBodyProps extends React.HTMLProps<HTMLTableSectionElement> {}
-
-interface TableCellProps extends React.HTMLProps<HTMLTableCellElement> {
-  className?: string;
-}
-
-const Table: React.FC<TableProps> = ({ className, ...props }) => (
+const Table: React.FC<React.ComponentPropsWithoutRef<"table">> = ({ className, ...props }) => (
   <table className={clsx("w-full border-collapse text-sm", className)} {...props} />
 );
 
-const TableHead: React.FC<TableHeadProps> = ({ className, ...props }) => (
+const TableHead: React.FC<React.ComponentPropsWithoutRef<"thead">> = ({ className, ...props }) => (
   <thead className={clsx("bg-gray-100 text-left font-medium", className)} {...props} />
 );
 
-const TableRow: React.FC<TableRowProps> = ({ className, ...props }) => (
+const TableRow: React.FC<React.ComponentPropsWithoutRef<"tr">> = ({ className, ...props }) => (
   <tr className={clsx("border-b", className)} {...props} />
 );
 
-const TableHeader: React.FC<TableHeaderProps> = ({ className, ...props }) => (
+const TableHeader: React.FC<React.ComponentPropsWithoutRef<"th">> = ({ className, ...props }) => (
   <th className={clsx("px-4 py-2 text-gray-700", className)} {...props} />
 );
 
-const TableBody: React.FC<TableBodyProps> = ({ ...props }) => <tbody {...props} />;
+const TableBody: React.FC<React.ComponentPropsWithoutRef<"tbody">> = (props) => <tbody {...props} />;
 
-const TableCell: React.FC<TableCellProps> = ({ className, ...props }) => (
+const TableCell: React.FC<React.ComponentPropsWithoutRef<"td">> = ({ className, ...props }) => (
   <td className={clsx("px-4 py-2", className)} {...props} />
 );
 
