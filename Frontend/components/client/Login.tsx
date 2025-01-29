d16414657c7c697c4c8ef77f9971e793c928f376
nidraoo <05nidzzzzz@gmail.com>
2025-01-30 05:26:32 +0530

    login verification

--- Frontend/components/client/Login.tsx
+++ Frontend/components/client/Login.tsx
@@ -33,6 +33,14 @@ export const Login = () => {
       localStorage.setItem("client", email); //hardcoded👀
       router.push("/client");
     }
+    else if(res.status == 201){
+      toast.error("Not verified");
+        return;
+    }
+    else if(res.status == 202){
+      toast.error("Not registered");
+      router.push("/client/register");
+    }
     else {
       toast.error("Login failed");
       return;
